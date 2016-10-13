64a762e9965e3ec9fd4e49d89e429ae4ac9214a6
simplu27 <simplu27@yahoo.com>
2016-10-13 17:38:56 +0300

    radio, select

--- index.php
+++ index.php
@@ -20,15 +20,51 @@
 <div class="center">
 	<div class="form-container">
 		<form id="formx" action="form-result.php" method="post" data-parsley-validate>
-			<label for="name">Name: <span>(Required field)</span></label><input type="text" id="name" name="name" required="" data-parsley-minlength="4">
+
+			<label for="name">Name: <span>(Required field)</span></label><input type="text" id="name" name="name" required="" data-parsley-minlength="4" placeholder="Ex: John Smith">
+		<br>
 			<label for="email">Email: <span>(Required field)</span></label><input type="email" id="email" name="email" required="">
-			<label for="url">Website: <span>(Optional field)</span></label><input type="text" id="url" name="url">
+		<br>
+			<label for="url">Website: <span>(Optional field)</span></label><input type="text" id="url" name="url" value="http://">
+		<hr>
+			<label for="heard">Heard about us via *:</label>
+			<select id="heard" name="heard" required="">
+				<option value="">Choose..</option>
+				<option value="press">Press</option>
+				<option value="net">Internet</option>
+				<option value="mouth">Word of mouth</option>
+				<option value="other">Other..</option>
+			</select>
+		<br>
+		<hr>
+			<label for="hobbies">Hobbies:</label>
+			<div class="checkbox-group">
+				<label><input name="hobbies[]" id="hobby1" value="ski" type="checkbox">Ski</label>
+			    <label><input name="hobbies[]" id="hobby2" value="run" type="checkbox" checked>Run</label>
+			    <label><input name="hobbies[]" id="hobby3" value="eat" type="checkbox">Eat</label>
+			    <label><input name="hobbies[]" id="hobby4" value="sleep" type="checkbox">Sleep</label>
+			    <label><input name="hobbies[]" id="hobby5" value="read" type="checkbox">Read</label>
+			    <label><input name="hobbies[]" id="hobby6" value="code" type="checkbox">Code</label>
+			</div><!-- checkbox-group -->
+		<hr>
+
+			<label >Radio Grup</label>
+			<div class='radio-container'>
+				<label><input type="radio"  name="radiox" value="Round1">Radio 1"</label><br>
+				<label><input type="radio"  name="radiox" value="Round2">Radio 2"</label><br>
+				<label><input type="radio"  name="radiox" value="Round3">Radio 3"</label><br>
+				<label><input type="radio"  name="radiox" value="Round4">Radio 4"</label><br>
+			</div>
+
+		<hr>
 			<label for="message">Message: <span>(Required field)</span></label>
-
+		<br>
 			<textarea name="message" id="message" cols="50" rows="4" required=""></textarea>
+		<br>
+			<input style="display:none;" type="text" id="fake" name="fake" value="x">
 
-			<input style="display:none;" type="text" id="fake" name="fake">
 			<input type="submit" name="submit" id="submit" value="Submit">
+
 		</form>	
 	</div><!-- form-container -->	
 </div><!-- center -->
